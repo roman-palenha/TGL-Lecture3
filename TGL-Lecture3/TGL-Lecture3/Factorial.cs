5fcb2c0e30f69497c88329b97ad2bd7db655bc52
roman-palenha <roman.palenha04@gmail.com>
2022-07-12 20:49:35 +0300

    added func

---
+++ TGL-Lecture3/TGL-Lecture3/Factorial.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Linq;
+
+namespace TGL_Lecture3
+{
+    public class Factorial
+    {
+        public int Find(int number)
+        {
+            if (number <= 0)
+                throw new ArgumentException("Wrong argument, must be > 0");
+            return Enumerable.Range(1, number).Aggregate((x, y) => x * y);
+        }
+    }
+}
---
+++ TGL-Lecture3/TGL-Lecture3/FibonacciSequence.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TGL_Lecture3
+{
+    public class FibonacciSequence
+    {
+        public IEnumerable<int> Make(int degree)
+        {
+            int prev = 0, current = 1, result;
+            return Enumerable.Range(0, degree + 1).Select(x =>
+            {
+                if (x <= 1)
+                {
+                    return x;
+                }
+                else
+                {
+                    result = prev + current;
+                    prev = current;
+                    current = result;
+                    return result;
+                }
+            });
+
+        }
+    }
+}
